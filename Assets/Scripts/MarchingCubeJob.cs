695f92d4364e641ef6f5f0354bc40be1772090ed
Sagar Patil <sagarreddypatil@gmail.com>
2021-10-17 04:31:48 -0400

    Working marching cubes

--- Assets/Scripts/MarchingCubeJob.cs
+++ Assets/Scripts/MarchingCubeJob.cs
@@ -1,58 +1,146 @@
+using System;
 using UnityEngine;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Burst;
+using System.Collections.Generic;
 using Unity.Collections;
+using System.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 
 public struct Triangle
 {
+    public bool created;
     public float3 c;
     public float3 b;
     public float3 a;
 }
 
-[BurstCompile]
+struct Cube : IEnumerable<float4>
+{
+    float4 _0;
+    float4 _1;
+    float4 _2;
+    float4 _3;
+    float4 _4;
+    float4 _5;
+    float4 _6;
+    float4 _7;
+
+    public float4 this[int index]
+    {
+        get
+        {
+            switch (index)
+            {
+            case 0:
+                return _0;
+            case 1:
+                return _1;
+            case 2:
+                return _2;
+            case 3:
+                return _3;
+            case 4:
+                return _4;
+            case 5:
+                return _5;
+            case 6:
+                return _6;
+            case 7:
+                return _7;
+            default:
+                throw new ArgumentOutOfRangeException($"Cube only has 8 edges. You tried to access edge ${index}");
+            }
+        }
+        set
+        {
+            switch (index)
+            {
+            case 0:
+                _0 = value;
+                break;
+
+            case 1:
+                _1 = value;
+                break;
+
+            case 2:
+                _2 = value;
+                break;
+
+            case 3:
+                _3 = value;
+                break;
+
+            case 4:
+                _4 = value;
+                break;
+
+            case 5:
+                _5 = value;
+                break;
+
+            case 6:
+                _6 = value;
+                break;
+
+            case 7:
+                _7 = value;
+                break;
+            }
+        }
+    }
+
+    public IEnumerator<float4> GetEnumerator()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            yield return this[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+// [BurstCompile]
 public struct MarchingCubesJob : IJobParallelFor
 {
     public float surfaceLevel;
     public int size;
-    public int metersPerCube;
     public float3 position;
     public float scale;
 
-    public NativeList<Triangle> triangles;
+    [NativeDisableParallelForRestriction]
+    public NativeArray<Triangle> triangles;
 
     public void Execute(int idx)
     {
-        int x = idx % size;
-        idx /= size;
-        int y = idx % size;
-        idx /= size;
-        int z = idx;
+        int tmpIdx = idx;
 
-        int3[] coords = {
-            new int3(x, y, z),
-            new int3(x + 1, y, z),
-            new int3(x + 1, y, z + 1),
-            new int3(x, y, z + 1),
-            new int3(x, y + 1, z),
-            new int3(x + 1, y + 1, z),
-            new int3(x + 1, y + 1, z + 1),
-            new int3(x, y + 1, z + 1),
-        };
+        int x = tmpIdx % size;
+        tmpIdx /= size;
+        int y = tmpIdx % size;
+        tmpIdx /= size;
+        int z = tmpIdx % size;
 
-        float4[] cube = new float4[8];
+        var cube = new Cube();
         int cubeIdx = 0;
 
         for (int i = 0; i < 8; i++)
         {
-            float3 realCoords = (float3)coords[i] * metersPerCube;
+            float3 realCoords = position + (float3)(new int3(x, y, z) + LUT.cornerCoords[i]) * scale;
             cube[i] = new float4(realCoords, noise(realCoords));
             if (cube[i].w < surfaceLevel)
             {
                 cubeIdx |= 1 << i;
             }
         }
+
+        int counter = 0;
 
         for (int i = 0; triTableValue(cubeIdx, i) != -1; i += 3)
         {
@@ -66,12 +154,22 @@ public struct MarchingCubesJob : IJobParallelFor
             int b2 = LUT.cornerIndexBFromEdge[triTableValue(cubeIdx, i + 2)];
 
             var newTri = new Triangle {
-                a = interpolateVerts(cube[a0], cube[b0]),
+                created = true,
+                c = interpolateVerts(cube[a0], cube[b0]),
                 b = interpolateVerts(cube[a1], cube[b1]),
-                c = interpolateVerts(cube[a2], cube[b2]),
+                a = interpolateVerts(cube[a2], cube[b2]),
             };
-
-            triangles.Add(newTri);
+            triangles[idx * 5 + counter] = newTri;
+            counter++;
+        }
+        for (int i = counter; i < 5; i++)
+        {
+            triangles[idx * 5 + i] = new Triangle {
+                created = false,
+                c = float3.zero,
+                b = float3.zero,
+                a = float3.zero
+            };
         }
     }
 
@@ -83,7 +181,7 @@ public struct MarchingCubesJob : IJobParallelFor
 
     int triTableValue(int a, int b)
     {
-        return LUT.triTable[16 * a + b];
+        return LUT.triTable[a * 16 + b];
     }
 
     float noise(float3 pos) // TODO: use noise from outside source, passed as parameter to this job
--- Assets/Scripts/MarchingCubes.cs
+++ Assets/Scripts/MarchingCubes.cs
@@ -1,19 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Jobs;
 
 public class MarchingCubes : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int size = 16;
+    public float scale = 0.1f;
+    public float surfaceLevel = 0f;
+
+    private MeshFilter meshFilter;
+    private Mesh mesh;
+
     void Start()
     {
-
+        Initialize();
+        GenerateMesh();
     }
 
-    // Update is called once per frame
+    void Initialize()
+    {
+        if (meshFilter == null)
+        {
+            meshFilter = GetComponent<MeshFilter>();
+
+            mesh = new Mesh();
+            mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+
+            meshFilter.sharedMesh = mesh;
+        }
+    }
+
+    void GenerateMesh()
+    {
+        var result = new NativeArray<Triangle>(size * size * size * 5, Allocator.Persistent, NativeArrayOptions.ClearMemory);
+        var job = new MarchingCubesJob { surfaceLevel = surfaceLevel,
+                                         size = size,
+                                         position = float3.zero,
+                                         scale = scale,
+                                         triangles = result };
+
+        var handle = job.Schedule(size * size * size, 1);
+        handle.Complete();
+
+        var triangleStructs = result.ToArray();
+        result.Dispose();
+
+        var vertices = new List<Vector3>();
+        var triangles = new List<int>();
+
+        for (int i = 0; i < triangleStructs.Length; i++)
+        {
+            if (triangleStructs[i].created)
+            {
+                vertices.Add(triangleStructs[i].a);
+                triangles.Add(vertices.Count - 1);
+
+                vertices.Add(triangleStructs[i].b);
+                triangles.Add(vertices.Count - 1);
+
+                vertices.Add(triangleStructs[i].c);
+                triangles.Add(vertices.Count - 1);
+            }
+        }
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+
     void Update()
     {
-
     }
 }
