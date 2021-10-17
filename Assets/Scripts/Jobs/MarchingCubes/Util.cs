4ac2d172cea8bbd538d58604fa3ae092284bd32e
Sagar Patil <sagarreddypatil@gmail.com>
2021-10-17 05:42:04 -0400

    minor changes

--- Assets/Scripts/Jobs/MarchingCubes/Util.cs
+++ Assets/Scripts/Jobs/MarchingCubes/Util.cs
@@ -1,9 +1,6 @@
 using System;
-using Unity.Jobs;
 using Unity.Mathematics;
-using Unity.Burst;
 using System.Collections.Generic;
-using Unity.Collections;
 using System.Collections;
 
 public struct Triangle
